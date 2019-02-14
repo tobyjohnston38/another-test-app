9fcc11866a11f03ce6aaaf1fd3f13239bd3f46db
Toby Johnston <toby@crssoftware.co.nz>
2019-02-14 13:15:21 +1300

    wip needs fix descendant issue

--- src/ContentComponent.js
+++ src/ContentComponent.js
@@ -24,20 +24,22 @@ const ContentComponent = props => {
       <div id="sidebar">
         <SidebarComponent
           sidebarOption={
-            <ul class="navigation">
-              <li>
-                <a href="thing.html">thing 1</a>
-              </li>
-              <li>
-                <a href="thing.html">thing 2</a>
-              </li>
-              <li>
-                <a href="thing.html">thing 3</a>
-              </li>
-              <li>
-                <a href="thing.html">thing 4</a>
-              </li>
-            </ul>
+            <div className="navigation">
+              <ul>
+                <li>
+                  <a href="thing.html">thing 1</a>
+                </li>
+                <li>
+                  <a href="thing.html">thing 2</a>
+                </li>
+                <li>
+                  <a href="thing.html">thing 3</a>
+                </li>
+                <li>
+                  <a href="thing.html">thing 4</a>
+                </li>
+              </ul>
+            </div>
           }
         />
       </div>
