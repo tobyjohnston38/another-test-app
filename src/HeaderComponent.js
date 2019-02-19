3c26cc4a268a4b5e11f3b78e3a217d98708f4643
Toby Johnston <toby@crssoftware.co.nz>
2019-02-19 17:19:31 +1300

    things

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import { checkboxChecked } from "./action"
+import { connect } from "react-redux"
 
 class HeaderComponent extends Component {
   state = {
     showParagraph: undefined,
     checkChecked: false,
     textBoxText: ""
-  };
+  }
 
   handleClick = () => {
-    this.setState({ showParagraph: !this.state.showParagraph });
-  };
+    this.setState({ showParagraph: !this.state.showParagraph })
+  }
 
-  handleCheckChecked(event) {
-    alert("Check this out!");
+  handleCheckChecked(event, action) {
+    // alert("Check this out!")
+    action()
   }
 
   handleTextBox = e => {
     this.setState({
       textBoxText: e.target.value
-    });
-  };
+    })
+  }
 
   render() {
     return (
@@ -34,15 +37,37 @@ class HeaderComponent extends Component {
           <h1>{`${this.state.textBoxText}`}</h1>
         </div>
         <div className="checkThis">
-          <input type="checkbox" onChange={this.handleCheckChecked} />
+          <input
+            type="checkbox"
+            onChange={event =>
+              this.handleCheckChecked(event, this.props.onCheckboxChecked)
+            }
+            checked={this.props.checked}
+          />
         </div>
         <button id={"button"} onClick={this.handleClick}>
           Click Me!
         </button>
         {this.state.showParagraph ? <p>{this.props.paragraphText}</p> : null}
       </>
-    );
+    )
   }
 }
 
-export default HeaderComponent;
+const mapDispatchToProps = () => {
+  return {
+    onCheckboxChecked: () => checkboxChecked()
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(`received the checked state ${state.checked}`)
+  return {
+    checked: state.checked
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HeaderComponent)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import rootReducer from "./reducer"
 
-ReactDOM.render(<App footerText={'good'}/>, document.getElementById('root'));
+const appState = createStore(rootReducer)
+
+const jsx = (
+  <Provider store={appState}>
+    <App footerText={"good"} />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
